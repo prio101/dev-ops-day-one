8c80007f067633b728f35af4da6e0b8155afa3c8
Mahabub Islam <miprio101@gmail.com>
2025-09-24 16:03:29 +0600

    added the script changes

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const port = 9000;
 
-app.get('/', (req, res)=> {
-
- console.log("hello world");
-})
-
+app.get("/", (req, res) => {
+  console.log("hello world", req);
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
